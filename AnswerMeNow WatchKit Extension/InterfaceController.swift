76cc16b54e472ee151dad46877f5d0abe6a9bfad
Jess <jesstelmanik@gmail.com>
2017-12-29 17:29:42 -0500

    Initial Commit

---
+++ AnswerMeNow WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  AnswerMeNow WatchKit Extension
+//
+//  Created by Jess Telmanik on 12/29/17.
+//  Copyright © 2017 Jess Telmanik. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
