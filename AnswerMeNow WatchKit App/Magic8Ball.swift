8a9a8ac336222306eb179bdaa93c674797fc82cf
Jess <jesstelmanik@gmail.com>
2018-01-24 10:49:11 -0500

    1 change to Array

--- AnswerMeNow WatchKit App/Magic8Ball.swift
+++ AnswerMeNow WatchKit App/Magic8Ball.swift
@@ -21,10 +21,9 @@ class Magic8BallIC:  WKInterfaceController {
     }
     
     func createAnswer() {
-        let eightBallArray: [String] = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+        let eightBallArray: [String] = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful", "This was a Demo"]
         print(eightBallArray.count)
         let random = Int(arc4random_uniform(UInt32(eightBallArray.count)))
         answerLabel.setText(eightBallArray[random])
     }
-    
 }
